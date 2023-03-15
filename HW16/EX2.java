fb887c92c35dbe59d3070bb5622c7470fa58c0ef
Diana <dianaturek95@gmail.com>
2023-03-15 01:13:24 +0100

    added files with homeworks

---
+++ HW16/EX2.java
@@ -0,0 +1,20 @@
+package HW16;
+
+import java.util.Scanner;
+
+/*public class EX2*/
+    /*public static void main(String[] args) {*/
+       /* Scanner input = new Scanner(System.in);
+        System.out.println("Введите текст:");
+        String string = input.nextLine();
+        String[] array = string.toLowerCase().split("а");
+
+        while ( int x=0; = array.length - 1;x >= 0;){
+            System.out.print(array.length);
+        }
+    }
+}*/
+
+    /*При помощи цикла while выполнить реализацию следующего алгоритма.
+        Пользователь вводит строку с клавиатуры. Программа должна вывести на
+        экран сколько символов "a", присутсвует в данной строке.*/
