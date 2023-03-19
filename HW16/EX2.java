fccdd29766a2c60f02debe6ce4050044723c3a73
Diana <dianaturek95@gmail.com>
2023-03-19 19:22:34 +0100

    added files with homeworks

--- HW16/EX2.java
+++ HW16/EX2.java
@@ -2,18 +2,23 @@ package HW16;
 
 import java.util.Scanner;
 
-/*public class EX2*/
-    /*public static void main(String[] args) {*/
-       /* Scanner input = new Scanner(System.in);
-        System.out.println("Введите текст:");
+public class EX2{
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Введите строку:");
         String string = input.nextLine();
-        String[] array = string.toLowerCase().split("а");
-
-        while ( int x=0; = array.length - 1;x >= 0;){
-            System.out.print(array.length);
+         int count = 0;
+        string = string.replaceAll("\\s","");
+        int index = string.indexOf("а");
+           while (index!=-1) {
+             count ++;
+             string = string.substring(index+1);
+             index = string.indexOf("а");
+           }
+         System.out.print(count);
         }
-    }
-}*/
+      }
+
 
     /*При помощи цикла while выполнить реализацию следующего алгоритма.
         Пользователь вводит строку с клавиатуры. Программа должна вывести на
